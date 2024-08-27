91deccf3a87b007bb2342903806e78f4c753b9a8
Andrew Santana <asantana91331@gmail.com>
2024-08-27 03:34:39 -0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    /**
+     * InnerHelloWorld
+     */
+    public class InnerHelloWorld {
+    
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+            
+
+        }
+    }
+}
